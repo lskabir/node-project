4c317e141ef9eb9d8b5528b6e6c516298744d077
Lutful Kabir <lutfulkbr@yahoo.com>
2022-01-30 11:51:28 -0500

    practice node

---
+++ index.js
@@ -0,0 +1,8 @@
+let superheroes = require("superheroes");
+let supervillains = require("supervillains");
+
+let mySuperHeroe = superheroes.random();
+let mySuperVillain = supervillains.random();
+
+console.log(mySuperHeroe);
+console.log(mySuperVillain);
